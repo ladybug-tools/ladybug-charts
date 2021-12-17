86a9443be901360cdbb56ee667383198c4b208bf
Devang <devngc@gmail.com>
2021-12-17 11:52:18 +0530

    style(to_figure_test): update function name

--- tests/to_figure_test.py
+++ tests/to_figure_test.py
@@ -1,10 +1,10 @@
 from ladybug_charts.to_figure import heatmap
-from ladybug_charts.to_df import create_df, DataPoint
+from ladybug_charts.to_dataframe import create_dataframe, DataPoint
 from plotly.graph_objects import Figure
 
 
 def test_heatmap():
     epw_path = r'tests/assets/weather/boston.epw'
-    df = create_df(epw_path)[0]
+    df = create_dataframe(epw_path)[0]
     fig = heatmap(df, DataPoint.relative_humidity)
     assert isinstance(fig, Figure)
