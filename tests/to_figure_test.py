83638b7be8c8ec059ee1137072565496ebbe6e7e
Devang <devngc@gmail.com>
2021-12-17 17:22:10 +0530

    refactor(to_figure_test): use ladybug-pandas

--- tests/to_figure_test.py
+++ tests/to_figure_test.py
@@ -1,10 +1,12 @@
-from ladybug_charts.to_figure import heatmap
-from ladybug_charts.to_dataframe import create_dataframe, DataPoint
+from ladybug_charts.to_figure import heatmap, DataPoint
+from ladybug_pandas.dataframe import DataFrame
+from ladybug.epw import EPW
 from plotly.graph_objects import Figure
 
 
 def test_heatmap():
     epw_path = r'tests/assets/weather/boston.epw'
-    df = create_dataframe(epw_path)[0]
+    epw = EPW(epw_path)
+    df = DataFrame.from_epw(epw)
     fig = heatmap(df, DataPoint.relative_humidity)
     assert isinstance(fig, Figure)
