3f8b5263a8d2b0de525d2c412ada2e2fd3b54392
Devang <devngc@gmail.com>
2021-12-17 22:33:28 +0530

    refactor(to_figure_test): update unit test

--- tests/to_figure_test.py
+++ tests/to_figure_test.py
@@ -1,12 +1,7 @@
-from ladybug_charts.to_figure import heatmap, DataPoint
-from ladybug_pandas.dataframe import DataFrame
 from ladybug.epw import EPW
 from plotly.graph_objects import Figure
 
 
 def test_heatmap():
-    epw_path = r'tests/assets/weather/boston.epw'
-    epw = EPW(epw_path)
-    df = DataFrame.from_epw(epw)
-    fig = heatmap(df, DataPoint.relative_humidity)
+    fig = EPW('tests/assets/weather/boston.epw').dry_bulb_temperature.heatmap()
     assert isinstance(fig, Figure)
