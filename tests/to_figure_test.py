4a6919d7b248bc76976e8fcd9c9e0677b70e0f43
Devang <devngc@gmail.com>
2021-12-21 14:24:55 +0530

    refactor(to_figure_test): add a unit test for HourlyDiscontinuousCollection

--- tests/to_figure_test.py
+++ tests/to_figure_test.py
@@ -2,6 +2,11 @@ from ladybug.epw import EPW
 from plotly.graph_objects import Figure
 
 
-def test_heatmap():
-    fig = EPW('tests/assets/weather/boston.epw').dry_bulb_temperature.heatmap()
+def test_hourly_continuous_to_heatmap(epw):
+    fig = epw.dry_bulb_temperature.heatmap()
+    assert isinstance(fig, Figure)
+
+
+def test_hourly_discontinuous_to_heatmap(epw):
+    fig = epw.dry_bulb_temperature.filter_by_conditional_statement('a>25').heatmap()
     assert isinstance(fig, Figure)
