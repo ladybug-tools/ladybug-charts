33055e224084b60550567ad2507fd06ddd0e9282
Devang <devngc@gmail.com>
2021-12-21 14:23:53 +0530

    feat(_extend_data): extend HourlyDiscontinuousCollection

--- ladybug_charts/_extend_data.py
+++ ladybug_charts/_extend_data.py
@@ -1,7 +1,8 @@
 """Add capability to generate a Figures from ladybug data objects. """
 
-from ladybug.datacollection import HourlyContinuousCollection
+from ladybug.datacollection import HourlyContinuousCollection, HourlyDiscontinuousCollection
 from ladybug_charts.to_figure import heatmap
 
 
 HourlyContinuousCollection.heatmap = heatmap
+HourlyDiscontinuousCollection.heatmap = heatmap
