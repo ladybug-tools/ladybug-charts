d236d3436745b172e08cb84faa075ef4b43d586b
Devang <devngc@gmail.com>
2021-12-22 16:43:30 +0530

    test(to_figure_test): add unit test for monthly data

--- tests/to_figure_test.py
+++ tests/to_figure_test.py
@@ -1,4 +1,3 @@
-from ladybug.epw import EPW
 from plotly.graph_objects import Figure
 
 
@@ -9,4 +8,9 @@ def test_hourly_continuous_to_heatmap(epw):
 
 def test_hourly_discontinuous_to_heatmap(epw):
     fig = epw.dry_bulb_temperature.filter_by_conditional_statement('a>25').heatmap()
+    assert isinstance(fig, Figure)
+
+
+def test_monthly_to_barchart(epw):
+    fig = epw.dry_bulb_temperature.average_monthly().barchart()
     assert isinstance(fig, Figure)
