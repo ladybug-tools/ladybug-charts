4d3ed75a4cb2e72f441b899324bb2bdcad0dbc45
Devang <devngc@gmail.com>
2021-12-22 20:11:18 +0530

    test(to_figure_test): add unit tests for barchart

--- tests/to_figure_test.py
+++ tests/to_figure_test.py
@@ -1,4 +1,9 @@
 from plotly.graph_objects import Figure
+from ladybug_comfort.degreetime import heating_degree_time, cooling_degree_time
+from ladybug.datacollection import HourlyContinuousCollection
+from ladybug.datatype.temperaturetime import HeatingDegreeTime, CoolingDegreeTime
+from ladybug_charts.to_figure import barchart
+from ladybug.color import Color
 
 
 def test_hourly_continuous_to_heatmap(epw):
@@ -13,4 +18,27 @@ def test_hourly_discontinuous_to_heatmap(epw):
 
 def test_monthly_to_barchart(epw):
     fig = epw.dry_bulb_temperature.average_monthly().barchart()
+    assert isinstance(fig, Figure)
+
+
+def test_barchart(epw):
+    dbt = epw.dry_bulb_temperature
+
+    _heat_base_ = 18
+    _cool_base_ = 23
+
+    hourly_heat = HourlyContinuousCollection.compute_function_aligned(
+        heating_degree_time, [dbt, _heat_base_],
+        HeatingDegreeTime(), 'degC-hours')
+    hourly_heat.convert_to_unit('degC-days')
+
+    hourly_cool = HourlyContinuousCollection.compute_function_aligned(
+        cooling_degree_time, [dbt, _cool_base_],
+        CoolingDegreeTime(), 'degC-hours')
+    hourly_cool.convert_to_unit('degC-days')
+
+    fig = barchart([hourly_heat.total_monthly(),
+                    hourly_cool.total_monthly()],
+                   chart_title='Degree-days',
+                   colors=[Color(255, 0, 0), Color(0, 0, 255)], stack=True)
     assert isinstance(fig, Figure)
