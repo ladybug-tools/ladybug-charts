f199748862e36260cb5f66141972f9f1d8a22143
Devang <devngc@gmail.com>
2021-12-22 16:41:53 +0530

    refactor(_extend_data): extend Monthly data with bar chart

--- ladybug_charts/_extend_data.py
+++ ladybug_charts/_extend_data.py
@@ -1,8 +1,9 @@
 """Add capability to generate a Figures from ladybug data objects. """
 
-from ladybug.datacollection import HourlyContinuousCollection, HourlyDiscontinuousCollection
-from ladybug_charts.to_figure import heatmap
+from ladybug.datacollection import HourlyContinuousCollection, HourlyDiscontinuousCollection, MonthlyCollection
+from ladybug_charts.to_figure import heatmap, monthly_barchart
 
 
 HourlyContinuousCollection.heatmap = heatmap
 HourlyDiscontinuousCollection.heatmap = heatmap
+MonthlyCollection.barchart = monthly_barchart
