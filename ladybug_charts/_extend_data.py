1b75cfe2fb8b8f6df29429c86017201c63e52b40
Devang <devngc@gmail.com>
2021-12-23 15:20:10 +0530

    feat(_extend_data): extend DailyCollection with a bar chart

--- ladybug_charts/_extend_data.py
+++ ladybug_charts/_extend_data.py
@@ -1,9 +1,11 @@
 """Add capability to generate a Figures from ladybug data objects. """
 
-from ladybug.datacollection import HourlyContinuousCollection, HourlyDiscontinuousCollection, MonthlyCollection
-from ladybug_charts.to_figure import heatmap, monthly_bar_chart
+from ladybug.datacollection import HourlyContinuousCollection, \
+    HourlyDiscontinuousCollection, MonthlyCollection, DailyCollection
+from ladybug_charts.to_figure import heatmap, monthly_bar_chart, daily_bar_chart
 
 
 HourlyContinuousCollection.heatmap = heatmap
 HourlyDiscontinuousCollection.heatmap = heatmap
 MonthlyCollection.barchart = monthly_bar_chart
+DailyCollection.barchart = daily_bar_chart
