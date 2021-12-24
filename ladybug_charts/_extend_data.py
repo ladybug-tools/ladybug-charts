ec6dcec5913693bea2706b909da3d8e5ef3dd25f
Devang <devngc@gmail.com>
2021-12-24 17:50:10 +0530

    feat(_extend_data): extend HourlyContinuousCollection with per hour chart

--- ladybug_charts/_extend_data.py
+++ ladybug_charts/_extend_data.py
@@ -2,11 +2,13 @@
 
 from ladybug.datacollection import HourlyContinuousCollection, \
     HourlyDiscontinuousCollection, MonthlyCollection, DailyCollection
-from ladybug_charts.to_figure import heatmap, monthly_bar_chart, daily_bar_chart, hourly_bar_chart
+from ladybug_charts.to_figure import heat_map, monthly_bar_chart, daily_bar_chart, \
+    hourly_bar_chart, per_hour_bar_chart
 
 
-HourlyContinuousCollection.heatmap = heatmap
-HourlyDiscontinuousCollection.heatmap = heatmap
-MonthlyCollection.barchart = monthly_bar_chart
-DailyCollection.barchart = daily_bar_chart
-HourlyContinuousCollection.barchart = hourly_bar_chart
+HourlyContinuousCollection.heat_map = heat_map
+HourlyDiscontinuousCollection.heat_map = heat_map
+MonthlyCollection.bar_chart = monthly_bar_chart
+DailyCollection.bar_chart = daily_bar_chart
+HourlyContinuousCollection.bar_chart = hourly_bar_chart
+HourlyContinuousCollection.per_hour_chart = per_hour_bar_chart
