789c4417fb288b89547d4b731431c7673cc62907
Devang <devngc@gmail.com>
2021-12-28 13:25:08 +0530

    style(*): renamed functions

--- ladybug_charts/_extend_data.py
+++ ladybug_charts/_extend_data.py
@@ -3,12 +3,12 @@
 from ladybug.datacollection import HourlyContinuousCollection, \
     HourlyDiscontinuousCollection, MonthlyCollection, DailyCollection
 from ladybug_charts.to_figure import heat_map, monthly_bar_chart, daily_bar_chart, \
-    hourly_bar_chart, per_hour_bar_chart
+    hourly_line_chart, per_hour_line_chart
 
 
 HourlyContinuousCollection.heat_map = heat_map
 HourlyDiscontinuousCollection.heat_map = heat_map
 MonthlyCollection.bar_chart = monthly_bar_chart
 DailyCollection.bar_chart = daily_bar_chart
-HourlyContinuousCollection.bar_chart = hourly_bar_chart
-HourlyContinuousCollection.per_hour_chart = per_hour_bar_chart
+HourlyContinuousCollection.line_chart = hourly_line_chart
+HourlyContinuousCollection.per_hour_line_chart = per_hour_line_chart
