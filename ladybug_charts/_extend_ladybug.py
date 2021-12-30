5bf18456b8ef917debf7d94da7b9c665f9b3db56
Devang <devngc@gmail.com>
2021-12-30 22:34:48 +0530

    feat(_extend_ladybug): extend psychrometric chart with a plot

--- ladybug_charts/_extend_ladybug.py
+++ ladybug_charts/_extend_ladybug.py
@@ -3,9 +3,9 @@
 from ladybug.datacollection import HourlyContinuousCollection, \
     HourlyDiscontinuousCollection, MonthlyCollection, DailyCollection
 from ladybug_charts.to_figure import heat_map, monthly_bar_chart, daily_bar_chart, \
-    hourly_line_chart, per_hour_line_chart, wind_rose
+    hourly_line_chart, per_hour_line_chart, wind_rose, psych_chart
 from ladybug.windrose import WindRose
-
+from ladybug.psychchart import PsychrometricChart
 
 HourlyContinuousCollection.heat_map = heat_map
 HourlyDiscontinuousCollection.heat_map = heat_map
@@ -14,3 +14,4 @@ DailyCollection.bar_chart = daily_bar_chart
 HourlyContinuousCollection.line_chart = hourly_line_chart
 HourlyContinuousCollection.per_hour_line_chart = per_hour_line_chart
 WindRose.plot = wind_rose
+PsychrometricChart.plot = psych_chart
