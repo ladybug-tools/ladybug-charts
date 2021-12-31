0842ee5f26ff8ea736868451fdb1fe1e600a9172
Devang <devngc@gmail.com>
2021-12-31 12:33:25 +0530

    feat(_extend_ladybug): extend hourlyplot and monthlychart

--- ladybug_charts/_extend_ladybug.py
+++ ladybug_charts/_extend_ladybug.py
@@ -3,9 +3,12 @@
 from ladybug.datacollection import HourlyContinuousCollection, \
     HourlyDiscontinuousCollection, MonthlyCollection, DailyCollection
 from ladybug_charts.to_figure import heat_map, monthly_bar_chart, daily_bar_chart, \
-    hourly_line_chart, per_hour_line_chart, wind_rose, psych_chart
+    hourly_line_chart, per_hour_line_chart, wind_rose, psych_chart, bar_chart
 from ladybug.windrose import WindRose
 from ladybug.psychchart import PsychrometricChart
+from ladybug.hourlyplot import HourlyPlot
+from ladybug.monthlychart import MonthlyChart
+
 
 HourlyContinuousCollection.heat_map = heat_map
 HourlyDiscontinuousCollection.heat_map = heat_map
@@ -15,3 +18,19 @@ HourlyContinuousCollection.line_chart = hourly_line_chart
 HourlyContinuousCollection.per_hour_line_chart = per_hour_line_chart
 WindRose.plot = wind_rose
 PsychrometricChart.plot = psych_chart
+
+
+def hourly_plot(self):
+    hourly_data = self.data_collection
+    return heat_map(hourly_data)
+
+
+HourlyPlot.plot = hourly_plot
+
+
+def plot_monthly_chart(self):
+    data = self.data_collections
+    return bar_chart(data)
+
+
+MonthlyChart.plot = plot_monthly_chart
